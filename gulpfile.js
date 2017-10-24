de6c868513bb986d81538866541bf73ae3b1c6b1
George Garside <grg.garside@gmail.com>
2017-10-24 13:07:01 +0100

    Comprehensive overhaul of gulpfile for ES6 & pages
    Add ES6 support to gulp, retain minification as ES5 for backwards compatibility, switched to babel for minification
    Use loop rather than individual sections for each page
    Add sourcemaps for JS and CSS
    Compile plugins into a single file
    Minify plugins JS and CSS only once on each first run of gulp, separated from watch to speed up recompilation (to recompile plugins, re-run gulp in Terminal)

--- gulpfile.js
+++ gulpfile.js
@@ -1,73 +1,92 @@
-var gulp      = require('gulp'),
-    plumber   = require('gulp-plumber'),
-    rename    = require('gulp-rename'),
-    uglify    = require('gulp-uglify'),
-    sass      = require('gulp-sass'),
-    path      = require('path'),
-    minifyCSS = require('gulp-minify-css')
-    concat    = require('gulp-concat');
+var gulp       = require('gulp'),
+    plumber    = require('gulp-plumber'),
+    rename     = require('gulp-rename'),
+    uglify     = require('gulp-uglify'),
+    sass       = require('gulp-sass'),
+    path       = require('path'),
+    minifyCSS  = require('gulp-minify-css'),
+    concat     = require('gulp-concat'),
+    sourcemaps = require('gulp-sourcemaps'),
+    babel      = require('gulp-babel'),
+    minifyJS   = require('gulp-babel-minify');
 
 gulp.task('scripts', function() {
-    gulp.src(['assets/js/plugins/**/*.js'])
-        .pipe(plumber())
-        .pipe(rename({
-            suffix: '.min'
-        }))
-        .pipe(uglify())
-        .pipe(gulp.dest('./public/js/plugins/'));
-
     // The Main files used on every page
     gulp.src(['assets/js/main.js', 'assets/js/pages/DynamicPage.js', 'assets/js/pages/Manager.js'])
         .pipe(plumber())
-        // .pipe(uglify())
+        .pipe(sourcemaps.init())
+        .pipe(babel({
+            presets: ['env']
+        }))
+        .pipe(minifyJS({
+	        mangle: false
+/*
+	        mangle: {
+		        keepClassName: true
+	        }
+*/
+        }))
         .pipe(concat('main.min.js'))
+        .pipe(sourcemaps.write('maps'))
         .pipe(gulp.dest('./public/js/'));
-
-    // Tickets
-    gulp.src(['assets/js/pages/tickets/**/*.js'])
-        .pipe(plumber())
-        // .pipe(uglify())
-        .pipe(concat('tickets.min.js'))
-        .pipe(gulp.dest('./public/js/pages/'));
-
-    // Staff
-    gulp.src(['assets/js/pages/staff/**/*.js'])
-        .pipe(plumber())
-        // .pipe(uglify())
-        .pipe(concat('staff.min.js'))
-        .pipe(gulp.dest('./public/js/pages/'));
-
-    // Hardware
-    gulp.src(['assets/js/pages/hardware/**/*.js'])
-        .pipe(plumber())
-        // .pipe(uglify())
-        .pipe(concat('hardware.min.js'))
-        .pipe(gulp.dest('./public/js/pages/'));
-
-    // Software
-    gulp.src(['assets/js/pages/software/**/*.js'])
-        .pipe(plumber())
-        // .pipe(uglify())
-        .pipe(concat('software.min.js'))
-        .pipe(gulp.dest('./public/js/pages/'));
-
-    // Global Metrics
-    gulp.src(['assets/js/pages/global_metrics/**/*.js'])
-        .pipe(plumber())
-        // .pipe(uglify())
-        .pipe(concat('global_metrics.min.js'))
-        .pipe(gulp.dest('./public/js/pages/'));
+    
+    for (var pagetype of ['tickets', 'staff', 'hardware', 'software', 'global_metrics']) {
+        gulp.src(['assets/js/pages/' + pagetype + '/**/*.js'])
+            .pipe(plumber())
+            .pipe(sourcemaps.init())
+            .pipe(babel({
+                presets: ['env']
+            }))
+            .pipe(minifyJS({
+	            mangle: false
+/*
+    	        mangle: {
+    		        keepClassName: true
+    	        }
+*/
+            }))
+            .pipe(concat(pagetype + '.min.js'))
+            .pipe(sourcemaps.write('maps'))
+            .pipe(gulp.dest('./public/js/pages/'));
+    }
 });
 
 gulp.task('styles', function() {
-    gulp.src('assets/sass/**/*.scss')
+    gulp.src(['assets/sass/**/*.scss', '!assets/sass/plugins/*.scss'])
         .pipe(plumber())
+        .pipe(sourcemaps.init())
         .pipe(sass())
         .pipe(rename({
             suffix: '.min'
         }))
         .pipe(minifyCSS())
+        .pipe(sourcemaps.write('maps'))
         .pipe(gulp.dest('./public/css/'));
+});
+
+gulp.task('plugins', function() {
+	  // Minify plugins once on first run of gulp
+	  // Separated from watch to speed up recompilation
+	  // Re-run gulp in Terminal after making changes to plugins
+    gulp.src(['assets/js/plugins/jquery.js', 'assets/js/plugins/jquery-ui.js', 'assets/js/plugins/jquery-timepicker-addon.js', 'assets/js/plugins/popper.js', 'assets/js/plugins/bootstrap.js', 'assets/js/plugins/bootstrap-select.js'])
+        .pipe(plumber())
+        .pipe(sourcemaps.init())
+        .pipe(concat('plugins.min.js'))
+        .pipe(minifyJS({
+	        mangle: {
+		        keepClassName: true
+	        }
+        }))
+        .pipe(sourcemaps.write('maps'))
+        .pipe(gulp.dest('./public/js/plugins/'));
+    gulp.src(['assets/sass/plugins/*.scss'])
+        .pipe(plumber())
+        .pipe(sourcemaps.init())
+        .pipe(sass())
+        .pipe(concat('plugins.min.css'))
+        .pipe(minifyCSS())
+        .pipe(sourcemaps.write('maps'))
+        .pipe(gulp.dest('./public/css/plugins/'));
 });
 
 gulp.task('watch', function() {
@@ -75,4 +94,4 @@ gulp.task('watch', function() {
     gulp.watch('assets/sass/**/*.scss', ['styles']);
 })
 
-gulp.task('default', ['styles', 'scripts', 'watch']);
+gulp.task('default', ['scripts', 'styles', 'plugins', 'watch']);
