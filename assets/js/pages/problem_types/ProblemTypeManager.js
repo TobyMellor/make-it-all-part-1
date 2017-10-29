d875c86bb799080162a545a2ab8fcca6c8ba2f25
TobyMellor <tobymulberry@hotmail.com>
2017-10-29 11:10:10 +0000

    Remove ticket references from the problem type page

--- assets/js/pages/problem_types/ProblemTypeManager.js
+++ assets/js/pages/problem_types/ProblemTypeManager.js
@@ -15,51 +15,68 @@ class ProblemTypeManager extends Manager {
 			new ProblemType(
 				1,
 				'Printers',
+				null,
 				[2, 8, 10]
 			),
 			new ProblemType(
 				2,
 				'Low Ink',
+				1,
 				[3, 4, 5, 6]
 			),
 			new ProblemType(
 				3,
 				'No Magenta',
+				2,
 				[]
 			),
 			new ProblemType(
 				4,
 				'No Yellow',
+				2,
 				[]
 			),
 			new ProblemType(
 				5,
 				'No Cyan',
+				2,
 				[]
 			),
 			new ProblemType(
 				6,
 				'No Black',
+				2,
 				[]
 			),
 			new ProblemType(
 				8,
 				'Computers',
+				null,
 				[9]
 			),
 			new ProblemType(
 				9,
 				'Display',
+				8,
 				[]
 			),
 			new ProblemType(
 				10,
 				'Software',
+				null,
 				[]
 			)
 		];
 
 		// loop through problem types, create instances and push to this.devices
+	}
+
+	getRootProblemTypes() {
+		return this.findAllWhere(this.problemTypes, problemType => problemType.parent === null);
+	}
+
+	getProblemType(problemTypeId) {
+		return this.findFirstWhere(this.problemTypes, problemType => problemType.id === problemTypeId);
 	}
 
 	getProblemTypes(problemTypeIds) {
--- assets/js/pages/problem_types/ProblemTypePage.js
+++ assets/js/pages/problem_types/ProblemTypePage.js
@@ -8,5 +8,35 @@
 class ProblemTypePage extends DynamicPage {
 	constructor() {
 		super();
+
+		this.loadSubProblemTypes($('.problem-type-picker .type-column')); // there will only be one at the start
+	}
+
+	loadSubProblemTypes($typeColumn, problemTypeId = null) {
+		var children = [];
+
+		if (problemTypeId === null) {
+			children = makeItAll.problemTypeManager.getRootProblemTypes();
+		} else {
+			var problemType = makeItAll.problemTypeManager.getProblemType(problemTypeId);
+
+			children = makeItAll.problemTypeManager.getProblemTypes(problemType.children);
+		}
+
+		console.log(children);
+
+		for (var i = 0; i < children.length; i++) {
+			var specialistCount = Math.floor(Math.random() * 5); // TODO: Replace with actual specialist count
+
+			$typeColumn.append(
+				'<li class="active">' +
+					children[i].name +
+					'<div class="specialist-counter">' +
+						(specialistCount > 0 ? specialistCount + ' <i class="fa fa-user"></i>' : '<i class="fa fa-user-times"></i>') +
+					'</div>' +
+					'<i class="fa fa-caret-right"></i>' +
+				'</li>'
+			);
+		}
 	}
 }
