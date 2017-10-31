81e30b63891bcbf1b0234ac590f4b3b7b16be4b3
George Garside <grg.garside@gmail.com>
2017-10-31 11:57:06 +0000

    Add list of permissions to Staff detail view

--- assets/js/pages/staff/StaffPage.js
+++ assets/js/pages/staff/StaffPage.js
@@ -27,6 +27,38 @@ class StaffPage extends DynamicPage {
 			el.textContent = String(Object.resolve(el.dataset.slug, employee)) || "—";
 		});
 		
+		$(this.detailSelector).find("[data-customslug]").each((i, el) => {
+			switch (el.dataset.customslug) {
+			
+				case "access":
+					el.innerHTML = "";
+					let icons = {read: "eye", operator: "user-secret", analyst: "line-chart", admin: "shield"};
+					
+					for (let permission of ["read", "operator", "analyst", "admin"]) {
+						
+						if (employee.access[permission]) {
+							
+							let elIcon = document.createElement("i");
+							elIcon.classList.add("fa", "fa-" + icons[permission]);
+							
+							let elText = document.createElement("span");
+							elText.textContent = permission.charAt(0).toUpperCase() + permission.slice(1);
+							
+							let elPermission = document.createElement("span");
+							elPermission.appendChild(elIcon);
+							elPermission.appendChild(elText);
+							
+							el.appendChild(elPermission);
+						}
+						
+					}
+					
+					break;
+				default:
+					el.textContent = "—";
+			}
+		});
+		
 		super.showTableRowDetails(id);
 	}
 }
