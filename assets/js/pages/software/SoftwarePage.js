384072d0ea2b62b9bbe58c76f019cefc9b50e356
George Garside <grg.garside@gmail.com>
2017-11-11 16:58:07 +0000

    Link from Software to Staff, Tickets and Hardware

--- assets/js/pages/software/SoftwarePage.js
+++ assets/js/pages/software/SoftwarePage.js
@@ -23,19 +23,19 @@ class SoftwarePage extends DynamicPage {
 
 		//Todo generate programmatically
 		$("#authorised-users").html(`
-		<li class="list-group-item list-group-item-action">
+		<li class="list-group-item list-group-item-action" data-rowid="0">
 			#0 Steve
 			<span class="pull-right text-muted">Operator</span>
 		</li>
-		<li class="list-group-item list-group-item-action">
+		<li class="list-group-item list-group-item-action" data-rowid="1">
 			#1 Andy
 			<span class="pull-right text-muted">Admin</span>
 		</li>
-		<li class="list-group-item list-group-item-action">
+		<li class="list-group-item list-group-item-action" data-rowid="2">
 			#2 Claire
 			<span class="pull-right text-muted">Analyst</span>
 		</li>
-		<li class="list-group-item list-group-item-action">
+		<li class="list-group-item list-group-item-action" data-rowid="3">
 			#3 Mary
 			<span class="pull-right text-muted">Specialist</span>
 		</li>
@@ -43,27 +43,27 @@ class SoftwarePage extends DynamicPage {
 
 
 		$("#tickets").html(`
-		<li class="list-group-item list-group-item-action">
+		<li class="list-group-item list-group-item-action" data-rowid="0">
 			#0: Printer Queue not working
 			<span class="filter filter-new">New</span>
 			<span class="pull-right text-muted">12/10/2017</span>
 		</li>
-		<li class="list-group-item list-group-item-action">
+		<li class="list-group-item list-group-item-action" data-rowid="1">
 			#1: Coffee machine needs refilling
 			<span class="filter filter-new">New</span>
 			<span class="pull-right text-muted">12/10/2017</span>
 		</li>
-		<li class="list-group-item list-group-item-action">
+		<li class="list-group-item list-group-item-action" data-rowid="2">
 			#2: Squeeky chair
 			<span class="filter filter-new">New</span>
 			<span class="pull-right text-muted">12/10/2017</span>
 		</li>
-		<li class="list-group-item list-group-item-action">
+		<li class="list-group-item list-group-item-action" data-rowid="3">
 			#3: Spilt water on macbook
 			<span class="filter filter-pending">Pending - In Progress</span>
 			<span class="pull-right text-muted">12/10/2017</span>
 		</li>
-		<li class="list-group-item list-group-item-action">
+		<li class="list-group-item list-group-item-action" data-rowid="4">
 			#4: Computer wont turn on
 			<span class="filter filter-resolved">Resolved</span>
 			<span class="pull-right text-muted">12/10/2017</span>
@@ -71,13 +71,13 @@ class SoftwarePage extends DynamicPage {
 		`);
 
 		$("#hardware").html(`
-			<li class="list-group-item list-group-item-action">
+			<li class="list-group-item list-group-item-action" data-rowid="0">
 				Laptop/MacBook Pro 2015
 			</li>
-			<li class="list-group-item list-group-item-action">
+			<li class="list-group-item list-group-item-action" data-rowid="1">
 				Laptop/ASUS
 			</li>
-			<li class="list-group-item list-group-item-action">
+			<li class="list-group-item list-group-item-action" data-rowid="2">
 				Desktop/HP
 			</li>
 		`);
